9e31e78e93438ac60729044f7bd3f3d74cedc5ea
iamhvsharma <itshvsharma@gmail.com>
2025-07-10 23:37:58 +0530

    fix

--- src/hooks/use-refetch.ts
+++ src/hooks/use-refetch.ts
@@ -1,15 +1,13 @@
-
-import { useQueryClient } from '@tanstack/react-query'
-import React from 'react'
+import { useQueryClient } from "@tanstack/react-query";
 
 const useRefetch = () => {
   const queryClient = useQueryClient();
 
   return async () => {
     await queryClient.refetchQueries({
-        type: 'active'
-    })
-  }
-}
+      type: "active",
+    });
+  };
+};
 
-export default useRefetch
+export default useRefetch;
