9625cccc97e165953a75ab82a89b7f863c699061
iamhvsharma <itshvsharma@gmail.com>
2025-07-16 20:42:53 +0530

    feat: Added a footer

---
+++ src/components/landing/Footer.tsx
@@ -0,0 +1,24 @@
+import Link from "next/link";
+import React from "react";
+
+const Footer = () => {
+  return (
+    <div>
+      <footer className="bg-yellow-metal-100 mt-auto py-6 text-gray-900">
+        <div className="container mx-auto text-center">
+          <p className="mt-2 text-sm font-semibold">
+            Made with ❤️ by Harshvardhan Sharma, Github:{" "}
+            <Link
+              href="https://github.com/iamhvsharma/codebrief"
+              className="hover:text-primary transition-colors"
+            >
+              @iamhvsharma
+            </Link>
+          </p>
+        </div>
+      </footer>
+    </div>
+  );
+};
+
+export default Footer;
